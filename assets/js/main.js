bf48709311c26c7a3749deb0ef712d7c21b78756
robsanbay <robsanbayisa@gmail.com>
2024-04-23 05:57:28 +0300

    products css done

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,8 +34,8 @@ navLink.forEach((n) => n.addEventListener("click", linkAction));
 /*=============== CHANGE BACKGROUND HEADER ===============*/
 function scrollHeader() {
   const header = document.getElementById("header");
-  // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
-  if (this.scrollY >= 100) header.classList.add("scroll-header");
+  // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+  if (this.scrollY >= 50) header.classList.add("scroll-header");
   else header.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
