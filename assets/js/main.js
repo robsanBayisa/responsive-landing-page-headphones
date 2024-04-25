cee942fc0a8cb49e250136d2637933b5f02ae1ba
robsanbay <robsanbayisa@gmail.com>
2024-04-25 03:08:03 +0300

    js done

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,3 +43,20 @@ window.addEventListener("scroll", scrollHeader);
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = scrollReveal({
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+sr.reveal(`.home__header, .section__title`, { delay: 600 });
+sr.reveal(`.home__footer`, { delay: 700 });
+sr.reveal(`.home__img`, { delay: 900, origin: `top` });
+
+sr.reveal(`.sponsor__img, .products__card, footer__logo, .footer__content, .footer__copy` { origin: `top`, interval: 100 });
+sr.reveal(`.specs__data, .discount__animate`, {
+  origin: `left`,
+  interval: 100,
+});
+sr.reveal(`.specs__img, .discount__img`, { origin: `right` });
+sr.reveal(`.case__img`, { origin: `top` });
+sr.reveal(`.case__data`);
