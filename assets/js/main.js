065bbf2a6d187b5f82a4b0cbeab4f598c9ed78c9
robsanbay <robsanbayisa@gmail.com>
2024-04-25 03:48:09 +0300

    js scrool done

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,11 +52,14 @@ sr.reveal(`.home__header, .section__title`, { delay: 600 });
 sr.reveal(`.home__footer`, { delay: 700 });
 sr.reveal(`.home__img`, { delay: 900, origin: `top` });
 
-sr.reveal(`.sponsor__img, .products__card, footer__logo, .footer__content, .footer__copy` { origin: `top`, interval: 100 });
+sr.reveal(
+  `.sponsor__img, .products__card, footer__logo, .footer__content, .footer__copy`,
+  { origin: "top", interval: 100 }
+);
 sr.reveal(`.specs__data, .discount__animate`, {
-  origin: `left`,
+  origin: "left",
   interval: 100,
 });
-sr.reveal(`.specs__img, .discount__img`, { origin: `right` });
-sr.reveal(`.case__img`, { origin: `top` });
+sr.reveal(`.specs__img, .discount__img`, { origin: "right" });
+sr.reveal(`.case__img`, { origin: "top" });
 sr.reveal(`.case__data`);
